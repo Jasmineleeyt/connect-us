17edf9ce9aa34925521de3b9a9aae08b2dcb6091
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-02-24 14:36:44 -0500

    Fixed created thought not pushed to the associated user bug

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,7 +6,6 @@ module.exports = {
     async getUsers(req, res) {
         try {
             const users = await User.find()
-              .select('-__v')
               .populate('thoughts')
               .populate('friends')
             
@@ -20,10 +19,8 @@ module.exports = {
     async getOneUser(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userId})
-              .select('-__v')
               .populate('thoughts')
               .populate('friends')
-
             if (!user) {
                 return res.status(404).json({ message: 'Unable to find the user with the ID provided.'})
             }
