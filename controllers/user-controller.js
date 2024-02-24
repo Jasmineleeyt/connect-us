f9c791d3eaa815a115a56e468f048fb37ba201a9
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-02-24 01:10:13 -0500

    Added update and delete user functionalities

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,10 +5,14 @@ module.exports = {
     // Get all users
     async getUsers(req, res) {
         try {
-            const users  = await User.find();
+            const users  = await User.find()
+              .select('-__v')
+              .populate('thoughts')
+              .populate('friends')
+            
             res.json((users));
         } catch (err) {
-            return res.status(500).json(err);
+            res.status(500).json(err);
         }
     },
 
@@ -16,15 +20,16 @@ module.exports = {
     async getOneUser(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userId})
-            .select('-__v')
-            .populate('thoughts')
-            .populate('friends')
+              .select('-__v')
+              .populate('thoughts')
+              .populate('friends')
 
             if (!user) {
-                return res.status(404).json({ message: 'Unable to find the user with the id provided.'})
+                return res.status(404).json({ message: 'Unable to find the user with the ID provided.'})
             }
+            res.json((user));
         } catch (err) {
-            return res.status(500).json(err);
+            res.status(500).json(err);
         }
     },
 
@@ -34,9 +39,42 @@ module.exports = {
             const user = await User.create(req.body);
             res.json(user);            
         } catch (err) {
-            return res.status(500).json(err);
+            res.status(500).json(err);
+        }
+    },
+
+    // Update a user
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true } 
+            )
+            if (!user) {
+                return res.status(404).json({ message: 'Unable to find the user with the ID provided.'})
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // Delete an user
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(404).json({ message: 'Unable to find the user with the ID provided.' });
+            }
+
+            await Thought.deleteMany({ _id: { $in: user.thoughts }})
+            res.json({ message: 'User and their thought(s) are deleted.'});
+        } catch (err) {
+            res.status(500).json(err);
         }
     },
     
 
-}
+};
